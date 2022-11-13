1b5599cb79e419dacabacff8e326dacf2aa4a4a7
yonmoyonmo <yonmosound@gmail.com>
2022-11-13 23:12:17 +0900

    sibal

--- newServer/model/Sugguestion.go
+++ newServer/model/Sugguestion.go
@@ -6,7 +6,7 @@ import (
 
 type Sugguestion struct {
 	Id              int       `json:"id" db:"id"`
-	SugguestionType string    `json:"type" db:"sugguestion_type"`
+	SugguestionType string    `json:"sugguestion_type" db:"sugguestion_type"`
 	SuggustionText  string    `json:"sugguestion_text" db:"sugguestion_text"`
 	CountLike       int       `json:"count_likes" db:"count_likes"`
 	CreatedAt       time.Time `json:"created_at" db:"created_at"`
