09a9ea47d1f63beddd773c01199381e0461024dc
yonmoyonmo <yonmosound@gmail.com>
2022-11-13 22:15:49 +0900

    deploy files

---
+++ newServer/run_smthp_server.sh
@@ -0,0 +1,6 @@
+docker stop new_smthp_server
+docker rm new_smthp_server
+docker rmi new_smalltalk_server:latest
+
+docker build -t new_smalltalk_server:latest .
+docker run --name new_smthp_server -d -p 5555:5555 new_smalltalk_server:latest
