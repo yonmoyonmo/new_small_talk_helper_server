b0fc0da7807b7dbdc648bf0b8e135b1b28695d93
yonmoyonmo <yonmosound@gmail.com>
2022-11-13 23:03:46 +0900

    fix typo

--- newServer/model/Sugguestion.go
+++ newServer/model/Sugguestion.go
@@ -6,8 +6,8 @@ import (
 
 type Sugguestion struct {
 	Id              int       `json:"id" db:"id"`
-	SugguestionType string    `json:"sugguestionText" db:"sugguestion_text"`
-	SuggustionText  string    `json:"type" db:"sugguestion_type"`
+	SugguestionType string    `json:"type" db:"sugguestion_type"`
+	SuggustionText  string    `json:"sugguestionText" db:"sugguestion_text"`
 	CountLike       int       `json:"count_likes" db:"count_likes"`
 	CreatedAt       time.Time `json:"created_at" db:"created_at"`
 }
